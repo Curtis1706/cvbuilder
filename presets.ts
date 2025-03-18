63676a2947654e6d7d02a5656584b5f0541f3ffa
Curtis1706 <155291568+Curtis1706@users.noreply.github.com>
2025-03-18 14:40:04 +0100

    Update presets.ts

--- presets.ts
+++ presets.ts
@@ -1,56 +1,56 @@
 import { Education, Experience, Hobby, Language, PersonalDetails, Skill } from '@/type';
 
 export const personalDetailsPreset: PersonalDetails = {
-    fullName: 'NOM Prenom',
-    email: 'mail@example.com',
-    phone: '+123456789',
-    address: '123, Benin',
+    fullName: '',
+    email: '',
+    phone: '',
+    address: '',
     photoUrl: '/profile.jpg',
-    postSeeking:'Dev fullstack',
-    description:`Passionné par le développement web, j'ai une solide expérience en création de plateformes web, optimisation des performances et intégration de solutions techniques`
+    postSeeking:'',
+    description:``
 };
 
 export const experiencesPreset: Experience[] = [
     {
         id: 'uuid-1',
-        jobTitle: 'Développeur Web',
-        companyName: 'Tech Solutions',
-        startDate: '2022-01-01',
-        endDate: '2023-01-01',
-        description: 'Développement d\'applications web en utilisant React et Node.js.'
+        jobTitle: '',
+        companyName: '',
+        startDate: '',
+        endDate: '',
+        description: ''
     },
     {
         id: 'uuid-2',
-        jobTitle: 'Chef de projet',
-        companyName: 'Innovatech',
-        startDate: '2020-06-01',
-        endDate: '2022-01-01',
-        description: 'Gestion de projets techniques, coordination des équipes de développement.'
+        jobTitle: '',
+        companyName: '',
+        startDate: '',
+        endDate: '',
+        description: ''
     }
 ];
 
 export const educationsPreset: Education[] = [
     {
         id: 'uuid-3',
-        degree: 'Master en Informatique',
-        school: 'Edu',
-        startDate: '2015-09-01',
-        endDate: '2018-06-01',
-        description: 'Spécialisation en développement web et bases de données.'
+        degree: '',
+        school: '',
+        startDate: '',
+        endDate: '',
+        description: ''
     }
 ];
 
 export const skillsPreset: Skill[] = [
-    { id: 'uuid-4', name: 'React.js' },
-    { id: 'uuid-5', name: 'Node.js' }
+    { id: 'uuid-4', name: '' },
+    { id: 'uuid-5', name: '' }
 ];
 
 export const languagesPreset: Language[] = [
-    { id: 'uuid-6', language: 'Anglais', proficiency: 'Avancé' },
-    { id: 'uuid-7', language: 'Français', proficiency: 'Débutant' }
+    { id: 'uuid-6', language: 'Anglais', proficiency: '' },
+    { id: 'uuid-7', language: 'Français', proficiency: '' }
 ];
 
 export const hobbiesPreset: Hobby[] = [
-    { id: 'uuid-8', name: 'Voyager' },
-    { id: 'uuid-9', name: 'Lire des livres' }
+    { id: 'uuid-8', name: '' },
+    { id: 'uuid-9', name: '' }
 ];
